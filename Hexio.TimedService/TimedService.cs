8edd5a8116d32f89a3e14b4acc0509a2e940ba08
Morten Ricki Rasmussen <Mortyhd@live.dk>
2019-12-29 15:03:51 +0100

    Fixed build

--- Hexio.TimedService/TimedService.cs
+++ Hexio.TimedService/TimedService.cs
@@ -41,7 +41,7 @@ namespace Hexio.TimedService
 
         protected override async Task ExecuteAsync(CancellationToken token)
         {
-            Log.Information("Starting timed service {Hexio.TimedService}", typeof(T).Name);
+            Log.Information("Starting timed service {TimedService}", typeof(T).Name);
 
             while (!token.IsCancellationRequested)
             {
@@ -59,7 +59,7 @@ namespace Hexio.TimedService
                     }
                     catch (Exception ex)
                     {
-                        Log.Error(ex, "Failed during execution of timed service {Hexio.TimedService}", typeof(T).Name);
+                        Log.Error(ex, "Failed during execution of timed service {TimedService}", typeof(T).Name);
                     }
                 }
                 await Task.Delay(_interval, token);
@@ -68,7 +68,7 @@ namespace Hexio.TimedService
         
         public override Task StopAsync(CancellationToken cancellationToken)
         {
-            Log.Information("Stopping timed service {Hexio.TimedService}", typeof(T).Name);
+            Log.Information("Stopping timed service {TimedService}", typeof(T).Name);
 
             return Task.CompletedTask;
         }
